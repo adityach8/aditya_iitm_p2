0ca32c6b6e31143fa4e281cd4c3bd87dabffbac2
adityach8 <21f2000168@ds.study.iitm.ac.in>
2024-12-15 13:10:02 +0530

    Update autolysis.py

--- autolysis.py
+++ autolysis.py
@@ -1,3 +1,19 @@
+# /// script
+# requires-python = ">=3.9"
+# dependencies = [
+#   "pandas",
+#   "seaborn",
+#   "matplotlib",
+#   "numpy",
+#   "scipy",
+#   "openai",
+#   "scikit-learn",
+#   "requests",
+#   "ipykernel",  # Required for running in Jupyter environments
+# ]
+# ///
+
+
 import os
 import pandas as pd
 import numpy as np
